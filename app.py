06e91dacaa86c4e6d07b2cd770da4e8482ceed97
rohitp1620 <rohitpatil.info3016@gmail.com>
2025-04-09 11:47:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "Hello from Docker Strategy in OpenShift from My pc!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
