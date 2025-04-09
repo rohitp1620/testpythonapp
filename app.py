38cba58a34585f20b8331be7f432d9d3b9b904d2
rohitp1620 <rohitpatil.info3016@gmail.com>
2025-04-09 12:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Hello from Docker Strategy in OpenShift from My pc!"
+   return "Hello from Docker Strategy in OpenShift from sudhanshuuuu!"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
